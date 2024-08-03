fdcdb1fbb1be2cb746a5646b13636af0f2243024
abd950 <jahabdulrahman1000@gmail.com>
2024-08-03 00:16:27 -0400

    finished code-along

---
+++ script.js
@@ -0,0 +1,15 @@
+let all = document.getElementById("details");
+let gallery = document.getElementById("gallery");
+
+function showImage(btn){
+
+btn.preventDefault();
+
+let Selection = dropdownMenu.value
+let source;
+
+
+}
+
+Select.addEventListener("click", showImage);
+
